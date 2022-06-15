96ae8c405b6753f6c1ea3e1a2f6390c830abc7c2
Chitrang GitHub <ichitrangjain@gmail.com>
2022-06-15 13:23:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getname());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "nane");
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("You Have Been Signed Out Successfully");
+      $(".data").css("display", "none");
+    });
+  }
